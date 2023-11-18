6892c9f6415ea3353a6c0eddf10ae5d2167058ac
janune13 <127921464+janune13@users.noreply.github.com>
2023-11-17 19:33:37 -0700

    Update ConsoleManager.java

--- src/application/ConsoleManager.java
+++ src/application/ConsoleManager.java
@@ -1,48 +1,50 @@
 package application;
 
-//ConsolePane class written by Jaylene Nunez
+//Written by Jaylene Nunez
 
-    public class ConsoleManager {
-        private Database database;
-        private Project selectedProject;
+public class ConsoleManager {
+    private Database database;
+    private Project selectedProject;
 
-     // Getter and Setter for selectedProject
-     public Project getSelectedProject() {
-         return selectedProject;
-     }
+    // Getter and Setter for selectedProject
+    public Project getSelectedProject() {
+        return selectedProject;
+    }
 
-     public void setSelectedProject(Project selectedProject) {
-         this.selectedProject = selectedProject;
-     }
+    public void setSelectedProject(Project selectedProject) {
+        this.selectedProject = selectedProject;
+    }
 
-        // Constructor
-        public ConsoleManager(Database d) {
-            database = d;
-        }
+    // Constructor
+    public ConsoleManager(Database d) {
+        database = d;
+    }
 
-        public Database getDatabase() {
-            return database;
-        }
+    public Database getDatabase() {
+        return database;
+    }
 
-        // Create a new effort log in the database
-        public boolean CreateNewEffortLog(int hours, int minutes) {
-            if (selectedProject != null) {
-                EffortLog newLog = new EffortLog(hours, minutes);
-                return database.addLog(newLog, selectedProject);
-            } else {
-                // Handle the case where no project is selected
-                return false;
-            }
-        }
-
-        // Overloaded method call for testing purposes
-        public boolean CreateNewEffortLog(int hours, int minutes, double weight, int bias) {
-            EffortLog newLog = new EffortLog(hours, minutes, weight, bias);
-            return database.addLog(newLog, database.getProject(0));
-        }
-
-        // Clear all effort logs in the database
-        public void ClearEffortLogs() {
-            database.clearEffortLogs(database.getProject(0));
+    // Create a new effort log in the database
+    public boolean createNewEffortLog(int hours, int minutes) {
+        if (selectedProject != null) {
+            EffortLog newLog = new EffortLog(hours, minutes);
+            return database.addLog(newLog, selectedProject);
+        } else {
+            // Handle the case where no project is selected
+            return false;
         }
     }
+
+    // Overloaded method call for testing purposes
+    public boolean createNewEffortLog(int hours, int minutes, double weight, int bias) {
+        EffortLog newLog = new EffortLog(hours, minutes, weight, bias);
+        return database.addLog(newLog, database.getProject(0));
+    }
+
+    // Clear all effort logs in the database
+    public void clearEffortLogs() {
+        if (selectedProject != null) {
+            database.clearEffortLogs(selectedProject);
+        }
+    }
+}
